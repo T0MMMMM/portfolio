2c362b92c773066b47ea9a35ac375257dae8fd4a
T0MMMMM <tf.34500@gmail.com>
2025-02-09 19:34:08 +0100

    portfolio v1

--- pages/index.js
+++ pages/index.js
@@ -127,18 +127,18 @@ export default function Home() {
             {data.aboutpara}
           </p>
         </div>
-        <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+        <div className="mt-10 laptop:mt-40 p-2 laptop:p-0">
           <div>
-            <h1 className="text-2xl text-bold">Contact.</h1>
+            <h1 className="tablet:m-10 text-2xl text-bold">Contact.</h1>
             <div className="mt-10">
-              <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              <h1 className="tablet:m-10 text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
                 LET&apos;S WORK
               </h1>
-              <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              <h1 className="tablet:m-10 text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
                 TOGETHER
               </h1>
               {/*<Button type="primary">Schedule a call</Button>*/}
-              <div className="mt-10">
+              <div className="tablet:m-10 mt-10">
                 <Socials />
               </div>
             </div>
