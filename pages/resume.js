25b1e10f1120649c896fca06569c86cca331aa78
T0MMMMM <tf.34500@gmail.com>
2025-02-09 19:03:48 +0100

    portfolio v1

--- pages/resume.js
+++ pages/resume.js
@@ -67,7 +67,7 @@ const Resume = () => {
                   )
                 )}
               </div>
-              <div className="mt-5">
+              {/*<div className="mt-5">
                 <h1 className="text-2xl font-bold">Education</h1>
                 <div className="mt-2">
                   <h2 className="text-lg">{resume.education.universityName}</h2>
@@ -121,7 +121,7 @@ const Resume = () => {
                     </div>
                   )}
                 </div>
-              </div>
+              </div>*/}
             </div>
           </div>
         )}
